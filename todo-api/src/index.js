b668c353099a4eb94cebd69322e3672001481bc3
Louis Lambeau <llambeau@quadrabee.com>
2020-12-14 20:59:57 +0100

    frontail to stream logs.

--- todo-api/src/index.js
+++ todo-api/src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { version } from '../package.json';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const app = express();
 const port = 3000;
+
 app.use(bodyParser.json());
+app.use(morgan('combined'))
 
 app.get('/version', (req, res) => {
   res.send({ name: 'TODOS API', version });
