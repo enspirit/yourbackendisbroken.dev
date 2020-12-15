c75113ed7da73be68924807def8ad1d61b78af83
ygu <yoann.guyot@enspirit.be>
2020-12-15 11:13:41 +0100

    Revert "Add todos webservices."
    This reverts commit f2fa66c48fd414025700b8c937fe99bf5a3299ef.

--- todo-api/src/index.js
+++ todo-api/src/index.js
@@ -5,37 +5,12 @@ import morgan from 'morgan';
 
 const app = express();
 const port = 3000;
-const todos = [{
-    id: 1, description: "one"
-  }, {
-    id: 2, description: "two"
-  }, {
-    id: 3, description: "three"
-  }, {
-    id: 4, description: "four"
-  }, {
-    id: 5, description: "five"
-  }
-];
 
 app.use(bodyParser.json());
 app.use(morgan('combined'))
 
 app.get('/version', (req, res) => {
   res.send({ name: 'TODOS API', version });
-});
-
-app.get('/todos', (req, res) => {
-  res.send(todos);
-});
-
-app.get('/todos/:id', (req, res) => {
-  const found = todos.find(todo => todo.id == req.params.id);
-  if (found) {
-    res.send(found);
-  } else {
-    res.sendStatus(404);
-  }
 });
 
 app.listen(port, () => {
