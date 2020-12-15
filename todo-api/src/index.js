ff9f1d5b20a3b987be213985f811eceeaf935d0e
Louis Lambeau <llambeau@quadrabee.com>
2020-12-15 22:10:18 +0100

    Step 1: broken backend :(

---
+++ todo-api/src/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const { version } = require('../package.json');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+const todos = [{
+  id: 1, description: 'one'
+}, {
+  id: 2, description: 'two'
+}, {
+  id: 3, description: 'three'
+}, {
+  id: 4, description: 'four'
+}, {
+  id: 5, description: 'five'
+}
+];
+
+function getTodos() {
+  return todos;
+}
+
+function getTodo(id) {
+  return getTodos().find(todo => todo.id == id);
+}
+
+function nextId() {
+  const ids = getTodos().map(t => t.id);
+  const lastId = Math.max(...ids);
+  return lastId + 1;
+}
+
+function addTodo(todo) {
+  const id = nextId();
+  getTodos().push({
+    id: id,
+    ...todo
+  });
+  return id;
+}
+
+function removeTodo(id) {
+  return getTodos().splice(id, 1);
+}
+
+app.use(bodyParser.json());
+app.use(morgan('combined'));
+
+app.get('/version', (req, res) => {
+  res.send({ name: 'TODOS API', version });
+});
+
+app.get('/todos', (req, res) => {
+  res.send(getTodos());
+});
+
+app.get('/todos/:id', (req, res) => {
+  res.send(getTodo(req.params.id));
+});
+
+app.post('/todos', (req, res) => {
+  const todo = req.body;
+  const id = addTodo(todo);
+  res.send(getTodo(id));
+});
+
+app.delete('/todos/:id', (req, res) => {
+  removeTodo(req.params.id);
+  res.sendStatus(204);
+});
+
+app.listen(port, () => {
+  console.log(`API listening at http://0.0.0.0:${port}`);
+});
