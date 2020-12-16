afcdddb6bdd77008941c1f285437bb403a6b51d7
Louis Lambeau <llambeau@quadrabee.com>
2020-12-16 22:54:51 +0100

    socket.io: send cmd to tmux.

--- .tutorial/api.js
+++ .tutorial/api.js
@@ -1,20 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const { exec } = require('child_process');
 
 const app = express();
 const http = require('http').createServer(app);
 const port = 8080;
+const TMUX_SESSION = process.env.YBIB_TMUX_SESS;
 
-app.use(bodyParser.json());
+// Serve website (see Dockerfile)
 app.use(express.static('public'));
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
-});
-
-console.log(process.env);
-
+// Socket.io for public website <> tutorial communication
 const io = require("socket.io")(http, {
   cors: {
     origin: "https://yourbackendisbroken.dev",
@@ -22,19 +18,32 @@ const io = require("socket.io")(http, {
   }
 });
 
+// Send commands to the main terminal the user uses
+const runCmd = (cmd) => {
+  console.log('running cmd > ', cmd);
+  // // Ctrl-C to make sure no user-typed command is pretyped
+  exec(`tmux send-keys -t ${TMUX_SESSION}.0 C-c`);
+  // // Send the actual command
+  // // TODO @llambeau: potential security issue here, even though we are in a docker
+  // //  controled env
+  exec(`tmux send-keys -t ${TMUX_SESSION}.0 "${cmd}" ENTER`);
+};
+
+// The public website pings 'localhost' to see if a tuto is running.
+// When a succesful connection is established, the website's UX is changing
+// slightly so that the tutorial shell is following the web-based tutorial
 io.on('connection', socket => {
+  // Confirm to the website we are there
   socket.emit('ping');
 
-  socket.on('previous', () => {
-    console.log('User wants previous');
+  // make sure the tutorial is on a specific step (aka: git tag)
+  socket.on('setStep', (num) => {
+    console.log('socketio#steStep', num);
+    runCmd(`./bin/step ${num}`);
   });
 
-  socket.on('next', () => {
-    console.log('User wants next');
-  });
-
-  socket.on('run', (cmd) => {
-    console.log('User wants to run', cmd);
+  socket.on('runCmd', (cmd) => {
+    runCmd(cmd);
   });
 });
 
