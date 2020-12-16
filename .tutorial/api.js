19b8c70454f4dc4bc835029624ac648eac05161c
Louis Lambeau <llambeau@quadrabee.com>
2020-12-16 17:31:51 +0100

    socketio

--- .tutorial/api.js
+++ .tutorial/api.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
+const http = require('http').createServer(app);
 const port = 8080;
 
 app.use(bodyParser.json());
@@ -10,7 +11,30 @@ app.use(express.static('public'));
 
 app.get('/ping', (req, res) => {
   res.send('pong');
-})
+});
+
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "https://yourbackendisbroken.dev",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', socket => {
+  socket.emit('ping');
+
+  socket.on('previous', () => {
+    console.log('User wants previous');
+  });
+
+  socket.on('next', () => {
+    console.log('User wants next');
+  });
+
+  socket.on('run', (cmd) => {
+    console.log('User wants to run', cmd);
+  });
+});
 
 app.use(createProxyMiddleware({
   target: 'http://localhost:9001',
@@ -18,6 +42,6 @@ app.use(createProxyMiddleware({
   ws: true
 }));
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Tutorial API listening at http://0.0.0.0:${port}`);
 });
