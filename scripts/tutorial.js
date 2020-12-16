af6669750a4bd0aac41d4dd391913d267ed92104
Louis Lambeau <llambeau@quadrabee.com>
2020-12-17 00:46:29 +0100

    Hook buttons to tutorial's API when relevant.

--- scripts/tutorial.js
+++ scripts/tutorial.js
@@ -1,37 +1,38 @@
 (function() {
   const socket = io('http://localhost:8080');
 
-  const hackButtons = function() {
-    // Next buttons
-    const nextBtns = document.getElementsByClassName('btn next')
-    for (let i = 0; i < nextBtns.length; i++) {
-      const btn = nextBtns[i];
-      if (btn.hacked) {
-        continue;
-      }
-      btn.addEventListener('click', () => {
-        sendCmd('next');
-      });
-      btn.hacked = true;
-    };
-
-
-    // Prev buttons
-    const prevBtns = document.getElementsByClassName('btn prev')
-    for (let i = 0; i < prevBtns.length; i++) {
-      const btn = prevBtns[i];
-      if (btn.hacked) {
-        continue;
-      }
-      btn.addEventListener('click', () => {
-        sendCmd('previous');
-      });
-      btn.hacked = true;
-    };
-  };
-
   const sendCmd = function(cmd, payload) {
     socket.emit(cmd, payload);
+  };
+
+  const setStep = (number) => {
+    sendCmd('setStep', number);
+  };
+
+  const hackButtons = function() {
+    // Next & previous buttons
+    const prevBtns = document.getElementsByClassName('btn prev')
+    const nextBtns = document.getElementsByClassName('btn next')
+    const btns = [].concat(
+      Array.prototype.slice.call(prevBtns),
+      Array.prototype.slice.call(nextBtns)
+    );
+
+    btns.forEach((btn) => {
+      if (btn.hacked) {
+        return;
+      }
+      btn.addEventListener('click', function(e) {
+        const target = this.attributes.href.value;
+        const regex = /tutorial\/step-([\d/+])/;
+        const match = target.match(regex);
+        if (match) {
+          const stepNumber = match[1];
+          setStep(stepNumber);
+        }
+      });
+      btn.hacked = true;
+    });
   };
 
   const ensureTutoMode = () => {
@@ -48,7 +49,13 @@
   };
 
   window.rerun = () => {
-    ensureTutoMode();
+    const STEP_REGEX = /\/tutorial\/step-(\d+)\.html/;
+    const matches = window.location.pathname.match(STEP_REGEX);
+    // Actually on a tutorial page
+    if (matches) {
+      const stepNumber = matches[1];
+      setStep(stepNumber);
+    }
   };
 
   socket.on('ping', () => {
