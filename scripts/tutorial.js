49934491dbe641770938839303595b21afab6f8b
Benjamin Ward <benward83@gmail.com>
2020-12-16 18:18:42 +0100

    tutorial.js basic button functionality

--- scripts/tutorial.js
+++ scripts/tutorial.js
@@ -1,2 +1,37 @@
+// Next button
+
+const nextBtns = document.getElementsByClassName('btn next')
 
 
+for (let i = 0; i < nextBtns.length; i++) {
+  nextBtns[i].addEventListener('click', () => {
+    alert('Clicked');
+  });
+};
+
+
+// Prev button
+
+const prevBtns =  document.getElementsByClassName('btn prev')
+
+for (let i = 0; i < prevBtns.length; i++) {
+  prevBtns[i].addEventListener('click', () => {
+    alert('Clicked');
+  });
+};
+
+
+//   Fetch
+
+const changePage = () => {
+  e.preventDefault();
+  fetch('http://madeupsite.com/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+  .then(data => console.log('success', data));
+};
