4202b9671832efc4bdb237fa92ae4fe9a22f8e69
Louis Lambeau <llambeau@quadrabee.com>
2020-12-16 17:21:31 +0100

    Refresh existing clone when mounted.

--- .tutorial/entrypoint.sh
+++ .tutorial/entrypoint.sh
@@ -3,6 +3,9 @@
 # Clone the repository if not present (mounted as volume for instance)
 if [ ! -d .git ]; then
   git clone --branch $YBIB_BRANCH $YBIB_REPO ./
+else
+  git remote update
+  git fetch --tags --force
 fi
 if [ $? -gt 0 ]; then
   echo "Cloning the git repository failed, please check your connection to internet."
