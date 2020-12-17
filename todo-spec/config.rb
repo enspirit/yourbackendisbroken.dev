b4fba28b19299baf26fda9de254d6cf3873c9ea8
Louis Lambeau <llambeau@quadrabee.com>
2020-12-17 16:28:25 +0100

    Step 7.

--- todo-spec/config.rb
+++ todo-spec/config.rb
@@ -1,4 +1,10 @@
 Webspicy::Configuration.new(Path.dir) do |c|
   c.host = "http://127.0.0.1:3000"
   c.client = Webspicy::HttpClient
+
+  c.precondition Webspicy::Specification::Pre::RobustToInvalidInput.new
+
+  c.before_each do |tester|
+    Kernel.system 'curl -X POST http://localhost:3000/reset'
+  end
 end
