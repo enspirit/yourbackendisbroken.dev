23794e512bbae076308d1406a075938bd7f70fd1
mdeserable <marie.deserable@enspirit.be>
2020-12-18 11:24:48 +0100

    fix the copy btn.

--- scripts/tutorial.js
+++ scripts/tutorial.js
@@ -14,6 +14,7 @@
       setTimeout(() => copyBtn.innerText = "Copy", 1000);
       copyBtn.innerText = "Copied :)";
     });
+    elm.hacked = true;
     elm.parentNode.insertBefore(copyBtn, elm.nextSibling);
   }
 
